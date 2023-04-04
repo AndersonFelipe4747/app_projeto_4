478b2db7d8540dc3ab7a51dc63b13b45eb7a6932
AndersonFelipe4747 <anderson.felipe4747@gmail.com>
2023-04-04 15:17:29 -0400

    04/04/2023
    Envio dos Projetos

---
+++ ProjetoPDS/Formularios/CadastrarContato.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ProjetoPDS
+{
+    partial class CadastrarContato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbTelefone = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.dtpDataNasc = new System.Windows.Forms.DateTimePicker();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.tbSexo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite o seu nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(584, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Selecione o seu sexo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(250, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Selecione a sua data de nascimento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Digite o seu email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(586, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Digite seu telefone:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(434, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cadastrar Contato";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(147, 109);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(338, 20);
+            this.tbNome.TabIndex = 7;
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.Location = new System.Drawing.Point(730, 109);
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(300, 20);
+            this.tbTelefone.TabIndex = 9;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(147, 160);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(338, 20);
+            this.tbEmail.TabIndex = 10;
+            // 
+            // dtpDataNasc
+            // 
+            this.dtpDataNasc.Location = new System.Drawing.Point(270, 247);
+            this.dtpDataNasc.Name = "dtpDataNasc";
+            this.dtpDataNasc.Size = new System.Drawing.Size(237, 20);
+            this.dtpDataNasc.TabIndex = 11;
+            // 
+            // btAdd
+            // 
+            this.btAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAdd.Location = new System.Drawing.Point(488, 319);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(94, 39);
+            this.btAdd.TabIndex = 13;
+            this.btAdd.Text = "ADD+";
+            this.btAdd.UseVisualStyleBackColor = false;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancelar.Location = new System.Drawing.Point(488, 409);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(94, 39);
+            this.btCancelar.TabIndex = 14;
+            this.btCancelar.Text = "CANCELAR";
+            this.btCancelar.UseVisualStyleBackColor = false;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalvar.Location = new System.Drawing.Point(488, 364);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(94, 39);
+            this.btSalvar.TabIndex = 15;
+            this.btSalvar.Text = "SALVAR";
+            this.btSalvar.UseVisualStyleBackColor = false;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // tbSexo
+            // 
+            this.tbSexo.Location = new System.Drawing.Point(741, 161);
+            this.tbSexo.Name = "tbSexo";
+            this.tbSexo.Size = new System.Drawing.Size(289, 20);
+            this.tbSexo.TabIndex = 16;
+            // 
+            // CadastrarContato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1066, 483);
+            this.Controls.Add(this.tbSexo);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.dtpDataNasc);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "CadastrarContato";
+            this.Text = "CadastrarContato";
+            this.Load += new System.EventHandler(this.CadastrarContato_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbTelefone;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.DateTimePicker dtpDataNasc;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.TextBox tbSexo;
+    }
+}
---
+++ ProjetoPDS/ProjetoPDS/Formularios/ListarContato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetoPDS.Contexto;
+using ProjetoPDS.RegrasDeNegocio;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using System.Security.Cryptography.X509Certificates;
+using ProjetoPDS.Formularios;
+
+namespace ProjetoPDS.Formularios
+{
+    public partial class ListarContato : Form
+    {
+        private MySqlConnection _conexao;
+        public ListarContato()
+        {
+            InitializeComponent();
+            Conexao();
+            CarregarDados();
+
+        }
+        public void Conexao()
+        {
+            string conexaoString = "server=localhost;database=app_contato_bd;user=root;password=root;port=3360";
+            _conexao = new MySqlConnection(conexaoString);
+            _conexao.Open();
+        }
+        public void CarregarDados()
+        {
+            var sql = "SELECT * FROM contato;";
+            MySqlCommand cmd = new MySqlCommand(sql, _conexao);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+            DataTable dataTable = new DataTable();
+            da.Fill(dataTable);
+            this.bsListarContato.DataSource = dataTable;
+        }
+    }
+}
